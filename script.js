45cbe718052978603ffbc7c6ddebd377348adf65
Reesejones1989 <mauricejonesjr@gmail.com>
2023-08-10 09:44:14 -0400

    added shuffle

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 
 let livesLeft = 10;
 var matches =0;
-
-// const boxHolder = document.querySelector(".outsideBox")
-
+let boxesCounted = 0;
 let  playerLives = document.querySelector(".livesLeft")
 playerLives.textContent = livesLeft;
-
 let playerMatches = document.querySelector(".matchesMade")
 playerMatches.textContent = matches;
+const resetButton = document.querySelector("resetButton")
+// let firstOpened=false;
+// let secondOpened=false;
 
-
-let boxesCounted = 0;
-
-const btn = document.querySelector("button")
-
-//  btn.addEventListener('click', function (e) {})
-let firstOpened=false;
-let secondOpened=false;
-
-//
 const imageArray =  [
 {imgSrc: "./images/pic1.webp", name:"pic1", id:1},
 {imgSrc: "./images/pic2.webp", name:"pic2", id:2},
@@ -35,27 +25,74 @@ const imageArray =  [
 {imgSrc: "./images/pic6.jpg", name:"pic6", id:12},
 ];
 
-// console.log(cardData);
-
-const outsideBox = document.querySelector(".outsideBox")
+const outsideBox = document.querySelector(".gameContainer")
+const insideBox = document.querySelector(".insideBox")
+var flippedCards = [];
+function shuffle(imageArray){
+  for (let i = imageArray.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [imageArray[i], imageArray[j]] = [imageArray[j], imageArray[i]];
+     }
+    }
 
 function newBoard(){
+    shuffle(imageArray);
+
 for (let i =0; i<imageArray.length; i++){
     let card = document.createElement('img')
     card.setAttribute('src', 'images/questionMark.png')
     card.setAttribute('id', i)
     // card.addEventListener("click", show)
     outsideBox.appendChild(card)
+    
+    
+    card.addEventListener("click", function (evt) {
+        // console.log(evt.target.id)
+        // console.log(imageArray[i].id -1)
+        card.setAttribute('src', imageArray[i].imgSrc)
+        card.setAttribute('name', imageArray[i].name)
+        // console.log(card.name)
+        flippedCards.push(evt.target.name)
+        console.log(flippedCards)
+
+        if(flippedCards[0] === flippedCards[1]){
+            console.log(`You found a match!`)
+            playerMatches.textContent++
+            // flippedCards[0].setAttribute('border', '5px solid yellow')
+            // flippedCards[1].setAttribute('border', '5px solid yellow')
+            
+            flippedCards=[];
+        }
+        else if (flippedCards.length >=2){ //NOT WORKING
+            // card.setAttribute('src', 'images/questionMark.png')
+            // console.log(evt.target.name)
+            flippedCards=[];
+            playerLives.textContent--
+            // return flippedCards
+            if (playerLives.textContent === 0){
+                console.log(playerLives.textContent)
+                alert('GAME OVER')
+            }
+        }
+       
+        // else if(flippedCards[0] !== flippedCards[1]){
+        //     playerLives.textContent--
+        // }
+     
+        // playerLives.textContent-- ; //change number of Lives
+      
+    })
+
+
+
+
+
 }
 }
-console.log(imageArray)
 
 newBoard();
 
 
-//Set the 12 boxes to have pic1-pic6 * 2
-// document.querySelectorAll("insideBox").values = cardData
-// const card=document.querySelectorAll("insideBox")
 // let cards = [...card]
 
 
@@ -66,30 +103,14 @@ newBoard();
 
 
 
-
-
-
-
-
-
-
-
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    return array;
+function restartGame(){
+    newBoard();
+    shuffle(imageArray);
 }
 
-
-
-
+// resetButton.addEventListener("click", function (e) {
+//     restartGame();
+//     console.log(e.target)
+    
+//     })
 
